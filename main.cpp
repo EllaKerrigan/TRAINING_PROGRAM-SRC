28f45b44183873a11bd79377fef3e3dbca489659
EllaKerrigan <129991492+EllaKerrigan@users.noreply.github.com>
2024-03-16 22:42:30 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,13 @@
 
 
 int main() {
-    sqlite3* db;
+    sqlite3* dataBase;
     const char* dbPath = "/Users/ellakerrigan/Downloads/SRC_DATABASE.db"; 
 
     // Open the SQLite database
-    int status = sqlite3_open(dbPath, &db);
+    int status = sqlite3_open(dbPath, &dataBase);
     if (status != SQLITE_OK) {
-        std::cerr << "Error opening database" << std::endl;
+        cerr << "Error opening database" << endl;
         return 1;
     }
 
@@ -20,20 +20,20 @@ int main() {
     sqlite3_stmt* statement;
     status = sqlite3_prepare_v2(db, sqlQuery, -1, &statement, nullptr);
     if (status != SQLITE_OK) {
-        std::cerr << "Error preparing statement" << std::endl;
-        sqlite3_close(db);
+        cerr << "Error preparing statement" << endl;
+        sqlite3_close(dataBase);
         return 1;
     }
 
     // get employee names and print
     while (sqlite3_step(statement) == SQLITE_ROW) {
         const unsigned char* name = sqlite3_column_text(statement, 0);
-        std::cout << "Employee Name: " << name << std::endl;
+        cout << "Employee Name: " << name << endl;
     }
 
     //  close database
     sqlite3_finalize(statement);
-    sqlite3_close(db);
+    sqlite3_close(dataBase);
 
     return 0;
 }
